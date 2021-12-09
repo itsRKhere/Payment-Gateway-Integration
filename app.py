38abd4e63dc123dbd57544212ba4978e55c02048
Rohit Kumar <89145032+itsRKhere@users.noreply.github.com>
2021-12-09 14:06:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from os import name
+from flask.wrappers import Request
+import razorpay
+
+from flask import Flask,render_template,request
+from razorpay.resources import payment
+app= Flask(__name__)
+
+@app.route('/')
+def mainpage():
+    return render_template("website.html")
+
+@app.route('/pay', methods=['POST'])
+def pay():
+    global payment,name,amount
+    name = request.form.get('username')
+    amount = request.form.get('amount')
+    client = razorpay.Client(auth=("rzp_test_uz9tIVoYhhhM0t","JfIv14cnSWoMaxIbbq0RDQLr"))
+
+    data = { "amount": int(amount)*100, "currency":"INR", "receipt": "#101" }
+    payment = client.order.create(data=data)
+    return render_template("pay.html",payment=payment)
+
+if __name__  == "__main__":
+    app.run(debug=True)
+
